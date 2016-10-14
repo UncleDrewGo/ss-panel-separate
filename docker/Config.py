0528d2e064f0862522d77e399e3a9b5e278a7d8e
wangjian <wj@umscloud.com>
2016-10-14 15:35:09 +0800

    update

--- docker/Config.py
+++ docker/Config.py
@@ -1,5 +1,5 @@
 #Config
-MYSQL_HOST = 'localhost'
+MYSQL_HOST = 'dockerMysql'
 MYSQL_PORT = 3306
 MYSQL_USER = 'ssuser'
 MYSQL_PASS = 'sspasswd'
@@ -7,6 +7,6 @@ MYSQL_DB = 'sspanel'
 
 MANAGE_PASS = 'ss233333333'
 #if you want manage in other server you should set this value to global ip
-MANAGE_BIND_IP = '127.0.0.1'
+MANAGE_BIND_IP = 'dockerMysql'
 #make sure this port is idle
 MANAGE_PORT = 23333
