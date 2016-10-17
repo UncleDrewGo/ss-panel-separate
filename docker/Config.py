06f85b37f53063095d841f7de516493bda7cf654
uncledrewgo <uncledrewgo@gmail.com>
2016-10-17 12:46:36 +0900

    update

--- docker/Config.py
+++ docker/Config.py
@@ -7,6 +7,6 @@ MYSQL_DB = 'sspanel'
 
 MANAGE_PASS = 'ss233333333'
 #if you want manage in other server you should set this value to global ip
-MANAGE_BIND_IP = 'dockerMysql'
+MANAGE_BIND_IP = '127.0.0.1'
 #make sure this port is idle
 MANAGE_PORT = 23333
